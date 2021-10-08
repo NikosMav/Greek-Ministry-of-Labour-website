096932033fde9b9bdace1518857603a4498868d0
Nikos Mavrapidis <33099084+NikosMav@users.noreply.github.com>
2021-10-09 02:45:41 +0300

    Delete ErrorMessage.php

--- ErrorMessage.php
+++
@@ -1,9 +0,0 @@
-
-<section style="height: 350px;">
-    <div style="width: 80%;margin-left: 10%;height: 10px;background-color: #b81111;"></div>
-    <div class="card" style="width: 80%;margin-left: 10%;height: 310px;">
-        <div class="card-body text-center"><i class="fas fa-times-circle" style="font-size: 80px;color: #b81111;margin-bottom: 33px;"></i>
-            <p class="card-text" style="font-size: 30px;"><strong>Σφάλμα!</strong><br><strong>Δεν έχετε πρόσβαση σε αυτή τη λειτουργία είτε επειδή δεν είστε συνδεδεμένος είτε επειδή δεν ανήκετε στην κατηγορία στην οποία αναφέρεται.</strong></p>
-        </div>
-    </div>
-</section>
