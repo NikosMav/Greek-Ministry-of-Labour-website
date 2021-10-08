2936f1d46fb9b318e55163589946ea5c0406ee97
Nikos Mavrapidis <33099084+NikosMav@users.noreply.github.com>
2021-10-09 02:46:11 +0300

    Delete NoLoginMessage.php

--- NoLoginMessage.php
+++
@@ -1,9 +0,0 @@
-
-<section style="height: 350px;">
-    <div style="width: 80%;margin-left: 10%;height: 10px;background-color: #b81111;"></div>
-    <div class="card" style="width: 80%;margin-left: 10%;height: 310px;">
-        <div class="card-body text-center"><i class="fas fa-times-circle" style="font-size: 80px;color: #b81111;margin-bottom: 33px;"></i>
-            <p class="card-text" style="font-size: 30px;"><strong>Σφάλμα!</strong><br><strong>Ο συνδυασμός Username και Password που εισάγατε δεν είναι σωστός.</strong></p>
-        </div>
-    </div>
-</section>
