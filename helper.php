561825c794eca990730e19f2d527daa81300b40e
Nikos Mavrapidis <33099084+NikosMav@users.noreply.github.com>
2021-10-09 02:48:25 +0300

    Delete helper.php

--- helper.php
+++
@@ -1,9 +0,0 @@
-<?php
-function getLoggedInID(){
-    if (isset($_SESSION['loggedIn']) && $_SESSION['loggedIn'] == true) {
-        return (int) $_SESSION['ID'];
-    } else {
-        return -1;
-    }
-}
-?>
